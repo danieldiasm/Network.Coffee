724a721851549bbf7e6fe32bc801385725c3dfeb
danieldiasm <daniel_diasm@hotmail.com>
2024-08-20 00:55:11 +0200

    implemented the serial start and print on main (to be changed)

--- m5_atom_dash/main.py
+++ m5_atom_dash/main.py
@@ -1,8 +1,15 @@
 from tunes import Tunes
 from player import Player
+from serial_comm import SerialComm
 
+ser_uart1 = SerialComm()
+ser_uart1.start_serial()
 
+Player.play_tune(Tunes.beep_beep_tune)
+print("READY!")
 
+while True:
+    data = ser_uart1.rcv_data()
+    if data:
+        print("Received data:", data)
 
-
-Player.play_tune(Tunes.schindler_lift_tune)
