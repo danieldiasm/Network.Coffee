5384bccde31be19935c70b0138fd2a3c5ab76cc4
danieldiasm <daniel_diasm@hotmail.com>
2024-08-20 00:53:43 +0200

    added base code to send and receive from uart

--- m5_atom_dash/serial_comm.py
+++ m5_atom_dash/serial_comm.py
@@ -1,14 +1,31 @@
 # Serial Communication Module
+from machine import UART
 
 class SerialComm:
 
-    def __init__(self, tx, rx, baud_rate) -> None:
+    def __init__(self, tx=22, rx=19, baud_rate=9600, timeout=1000) -> None:
         self.tx = tx
         self.rx = rx
         self.baud_rate = baud_rate
+        self.uart_id = 1
+        self.timeout = timeout
+        self.uart = UART(self.uart_id, 
+                         self.baud_rate,
+                         tx=self.tx,
+                         rx=self.rx)
+
+    def start_serial(self):
+        self.uart.init()
 
     def rcv_data(self):
-        pass
+        if self.uart.any():
+            data = self.uart.read()
+            if data:
+                return data.decode('utf-8')
+        return None
+    
+    def send_data(self, data):
+        self.uart.write(data)
 
-    def send_data(self):
+    def decode_data(self):
         pass
