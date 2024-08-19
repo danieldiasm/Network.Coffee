0a6a085b11596df268b33249f7ce19f4d0523a5c
danieldiasm <daniel_diasm@hotmail.com>
2024-08-20 00:54:40 +0200

    added a quicker boot up sound

--- m5_atom_dash/tunes.py
+++ m5_atom_dash/tunes.py
@@ -1,5 +1,6 @@
 # Pre-Defined Tunes
 
 class Tunes:
+    beep_beep_tune = [('A4',0.3),('D4',0.3)]
     schindler_lift_tune = [('A4',1),('F#4',1),('D4',1)]
     pitfall_death_tune = [('C#3',0.536),('D#3',0.183),('E3',0.364),('C#3',0.364),('G3',0.727)]
