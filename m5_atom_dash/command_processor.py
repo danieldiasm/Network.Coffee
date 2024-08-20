a09d292712f1a97eb7c1260f11f0d956bd0deca6
danieldiasm <daniel_diasm@hotmail.com>
2024-08-20 21:34:28 +0200

    added command lenght option and docstrings

--- m5_atom_dash/command_processor.py
+++ m5_atom_dash/command_processor.py
@@ -7,13 +7,25 @@ class CommandProcessorError(Exception):
 
 class CommandProcessor:
 
-    def __init__(self, command_methods: dict) -> None:
+    def __init__(self ,command_methods: dict, command_length:int = 1) -> None:
+        ''' 
+            Receives a dictionary for the commands formed with 1 character and the callable method,
+            and the length of the command string as an integer number(all commands must have this length).
+            Example of the command string:
+            {"P":print, "S":sum, "E":eval}
+            Returns an instance of a command processor that can route a message to those methods.
+        '''
         self.command_methods = command_methods
+        self.command_length = command_length
     
     def route(self, message: str):
-        command = message[:1]
-        data = message[1:]
-
+        '''
+            Receives a message with its first character being the command expected to be interpreted,
+            and the rest of the string as the data that will be provided as argument to the method called.
+        '''
+        command = message[:self.command_length]
+        data = message[self.command_length:]
+        # Possible improvement, receive multiple arguments
         try:
             self.command_methods[command](data)
             return None
