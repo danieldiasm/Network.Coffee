27f4ce5ee6781a49dc423b896bd7a53c53b1497b
danieldiasm <daniel_diasm@hotmail.com>
2024-08-20 20:19:07 +0200

    some example code that I've been using to test other code

--- m5_atom_dash/main.py
+++ m5_atom_dash/main.py
@@ -5,11 +5,18 @@ from serial_comm import SerialComm
 ser_uart1 = SerialComm()
 ser_uart1.start_serial()
 
-Player.play_tune(Tunes.beep_beep_tune)
 print("READY!")
 
 while True:
-    data = ser_uart1.rcv_data()
-    if data:
-        print("Received data:", data)
+    if ser_uart1.uart.any():
+        print("Incomming data...")
+        data = ser_uart1.rcv_data()
 
+        if data.startswith("T"):
+        try:
+            final_data = eval(data)
+            if isinstance(final_data, ):
+                print("It is a Tunes instance!")
+                Player.play_tune(final_data)
+        except NameError:
+            print("Not a valid command, ignoring")
