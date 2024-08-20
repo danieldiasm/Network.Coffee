8564d09829f43418710f00d63dbc6740b6f466da
danieldiasm <daniel_diasm@hotmail.com>
2024-08-20 22:05:21 +0200

    added error class and player eval to process lists comming as str

--- m5_atom_dash/player.py
+++ m5_atom_dash/player.py
@@ -4,6 +4,10 @@ from machine import PWM
 import time
 from notes import note_freq
 
+class PlayerError(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
 class Player:
     
     def play_tone(frequency, duration, duty=32768):
@@ -12,6 +16,13 @@ class Player:
         pwm.duty_u16(duty)  # Duty cycle for volume control
         time.sleep(duration)
         pwm.deinit()
+    
+    def eval_and_play(data):
+        eval_tune = eval(data)
+        if isinstance(eval_tune, list) and isinstance(eval_tune[0], tuple):
+            Player.play_tune(eval_tune)
+        else:
+            raise PlayerError("Invalid Tune provided")
 
     def play_tune(tune, duty = 16384):
         for note in tune:
