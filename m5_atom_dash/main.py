2f61c9433a68e585d3e9804fb89c481fc1d9e465
danieldiasm <daniel_diasm@hotmail.com>
2024-08-20 22:05:36 +0200

    implemented the new code

--- m5_atom_dash/main.py
+++ m5_atom_dash/main.py
@@ -1,22 +1,25 @@
 from tunes import Tunes
 from player import Player
 from serial_comm import SerialComm
+from command_processor import CommandProcessor
 
+# Starts Serial
 ser_uart1 = SerialComm()
 ser_uart1.start_serial()
+
+# Starts Command Processor
+com_processor = CommandProcessor(
+    {
+        "T":Player.eval_and_play,
+     }
+)
 
 print("READY!")
 
 while True:
-    if ser_uart1.uart.any():
+    if ser_uart1.incomming_data():
         print("Incomming data...")
         data = ser_uart1.rcv_data()
+        print(data)
+        com_processor.route(data)
 
-        if data.startswith("T"):
-        try:
-            final_data = eval(data)
-            if isinstance(final_data, ):
-                print("It is a Tunes instance!")
-                Player.play_tune(final_data)
-        except NameError:
-            print("Not a valid command, ignoring")
